1513e1f2318f49f105380575c7fe5e6ceb5dec80
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-04-15 20:31:46 -0500

    Stepper Demo with 3 simple steps

---
+++ lib/stepper.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class StepperDemo extends StatefulWidget {
+  StepperDemo() : super();
+
+  final String title = 'Stepper Demo';
+
+  @override
+  StepperDemoState createState() => StepperDemoState();
+}
+
+class StepperDemoState extends State<StepperDemo> {
+  int current_step = 0;
+
+  List<Step> steps = [
+    Step(
+      title: Text('Step 1'),
+      content: Text('Step 1 Content'),
+      isActive: true,
+    ),
+    Step(
+      title: Text('Step 2'),
+      content: Text('Step 2 Content'),
+      isActive: true,
+    ),
+    Step(
+      title: Text('Step 3'),
+      content: Text('Step 3 Content'),
+      state: StepState.complete,
+      isActive: true,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Simple Stepper Demo'),
+      ),
+      body: Container(
+        child: Stepper(
+          currentStep: this.current_step,
+          steps: steps,
+          type: StepperType.vertical,
+          onStepTapped: (step) {
+            setState(() {
+              current_step = step;
+            });
+          },
+          onStepContinue: () {
+            setState(() {
+              if (current_step < steps.length - 1) {
+                current_step = current_step + 1;
+              } else {
+                current_step = 0;
+              }
+            });
+          },
+          onStepCancel: () {
+            setState(() {
+              if (current_step > 0) {
+                current_step = current_step - 1;
+              } else {
+                current_step = 0;
+              }
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
